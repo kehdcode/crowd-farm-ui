f37b24c306a48dd9dd4f6703132bd9e4998ee76a
kehdcode <you@example.com>
2020-02-14 18:49:24 -0800

    price and service compnonet

---
+++ farm_crowdy/src/components/Price.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+ function Price() {
+    return (
+        <div>
+            <div className="row">
+                <div className="container-fluid">
+                    <div className="text-center">
+                        <h1>Our <strong>Pricing</strong></h1>
+                        <p>Lorem ipsum dolor sit amet, labores dolorum scriptorem eum an, te quodsi sanctus neglegentur.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="container-fluid">
+                        <div className="col-md-3">
+                            <h1>Hello 1</h1>
+                        </div>
+                        <div className="col-md-3">
+                            <h1>Hello 1</h1>
+                        </div>
+                        <div className="col-md-3">
+                            <h1>Hello 1</h1>
+                        </div>
+                        <div className="col-md-3">
+                            <h1>Hello 1</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+             
+        
+        </div>
+        
+    )
+}
+
+export default Price;
--- farm_crowdy/src/components/Service.js
+++ farm_crowdy/src/components/Service.js
@@ -1,12 +1,64 @@
 import React from 'react'
+import Price from './Price';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
  function Service() {
+    const element = <FontAwesomeIcon icon={faCoffee} />
     return (
         <section id="service">
             <div className="container">   
                 <div className="row">
+                <div className="col-md-4">
+                <div class="box flyLeft">
+                  <div class="icon">
+                  
+                  </div>
+                  <div class="text">
+                    <h4>High <strong>Quality</strong></h4>
+                    <p>
+                      Lorem ipsum dolor sit amet, has ei ipsum scaevola deseruisse am sea facilisis.
+                    </p>
+                    <a href="#">Learn More</a>
+                  </div>
+                </div>
+                </div>
+
+                <div className="col-md-4">
+                <div class="box flyLeft">
+                  <div class="icon">
+                    <i class="ico icon-circled icon-bgdark icon-star active icon-3x"></i>
+                  </div>
+                  <div class="text">
+                    <h4>High <strong>Quality</strong></h4>
+                    <p>
+                      Lorem ipsum dolor sit amet, has ei ipsum scaevola deseruisse am sea facilisis.
+                    </p>
+                    <a href="#">Learn More</a>
+                  </div>
+                </div>
+                </div>
+
+                <div className="col-md-4">
+                <div class="box flyLeft">
+                  <div class="icon">
+                    <i class="ico icon-circled icon-bgdark icon-star active icon-3x"></i>
+                  </div>
+                  <div className="text">
+                    <h4>High <strong>Quality</strong></h4>
+                    <p>
+                      Lorem ipsum dolor sit amet, has ei ipsum scaevola deseruisse am sea facilisis.
+                    </p>
+                    <a href="#">Learn More</a>
+                  </div>
+                </div>
+                </div>
+                
                  </div>
             </div>
+            
+            <Price />
                     
         </section>
     )
