2744cdb6fbf90235e98ade1920e391634155b3b1
kehdcode <you@example.com>
2020-02-18 18:29:14 -0800

    workign on testimony corousel

--- farm_crowdy/src/components/Testimonials.js
+++ farm_crowdy/src/components/Testimonials.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
 
  function Testimonials() {
     return (
@@ -6,32 +8,67 @@ import React from 'react'
             <div className="row">
                 <div className="container">
                     <div className="col-md-12 text-center">
-                    <h3 class="title">What People <strong>SAYING</strong> About Us</h3>
+                      <h3 class="title">What People <strong>SAYING</strong> About Us</h3>
                     </div>
-                    <ul>
-                    <li>
-                <blockquote>
-                  Aliquam a orci quis nisi sagittis sagittis. Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi lacinia sed interdum lorem vulputate. Aliquam a orci quis nisi sagittis sagittis. Etiam adipiscing, justo quis
-                  feugiat
-                </blockquote>
-                <div class="testimonial-autor">
-                  <img src="img/dummies/testimonial/1.png" alt="" />
-                  <h4>Hillary Doe</h4>
-                  <a href="#">www.companyname.com</a>
+                    <div className="container">
+                      <div className="row">
+                      <div class="seprator"></div>
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              
+              <div class="carousel-inner">
+                <div class="item active">
+                  <div class="row" style="padding: 20px">
+                    <button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+                    <p class="testimonial_para">Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. 
+                    Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo en.</p><br>
+                    <div class="row">
+                    <div class="col-sm-2">
+                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/jack.jpg" class="img-responsive" style="width: 80px"></img>
+                        </div>
+                        <div class="col-sm-10">
+                        <h4><strong>Jack Andreson</strong></h4>
+                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span><br/>
+                        <span>Officeal All Star Cafe</span>
+                        </p>
+                    </div>
+                    </div>
+                  </div>
                 </div>
-              </li>
-              <li>
-                <blockquote>
-                  Aliquam a orci quis nisi sagittis sagittis. Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi lacinia sed interdum lorem vulputate. Aliquam a orci quis nisi sagittis sagittis. Etiam adipiscing, justo quis
-                  feugiat
-                </blockquote>
-                <div class="testimonial-autor">
-                  <img src="img/dummies/testimonial/2.png" alt="" />
-                  <h4>Michael Doe</h4>
-                  <a href="#">www.companyname.com</a>
+               <div class="item">
+                   <div class="row">
+                    <button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
+                    <p class="testimonial_para">Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo en.</p><br>
+                    <div class="row">
+                    <div class="col-sm-2">
+                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/kiara.jpg" class="img-responsive" style="width: 80px"></img>
+                        </div>
+                        <div class="col-sm-10">
+                        <h4><strong>Kiara Andreson</strong></h4>
+                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span>
+                        <br/>
+                        <span>Officeal All Star Cafe</span>
+                        </p>
+                    </div>
+                    </div>
+                  </div>
+                  </div>
                 </div>
-              </li>
-                    </ul>
+              </div>
+            </div>
+            <div class="controls testimonial_control pull-right">
+                <a class="left fa fa-chevron-left btn btn-default testimonial_btn" href="#carousel-example-generic"
+                  data-slide="prev"></a>
+
+                <a class="right fa fa-chevron-right btn btn-default testimonial_btn" href="#carousel-example-generic"
+                  data-slide="next"></a>
+              </div>
+        </div>
+                        
+                      </div>
+
+                    </div>
+                    
                 </div>{/*End of container */}
 
             </div>{/*End of row */}
