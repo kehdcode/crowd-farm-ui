a69e4d574b47c32145a1b2308c43bb2d48acbe8b
kehdcode <you@example.com>
2020-02-18 18:37:54 -0800

    testimonial coursel

--- farm_crowdy/src/components/Testimonials.js
+++ farm_crowdy/src/components/Testimonials.js
@@ -5,30 +5,25 @@ import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
  function Testimonials() {
     return (
         <div>
-            <div className="row">
-                <div className="container">
-                    <div className="col-md-12 text-center">
-                      <h3 class="title">What People <strong>SAYING</strong> About Us</h3>
-                    </div>
-                    <div className="container">
-                      <div className="row">
-                      <div class="seprator"></div>
+            <div class="container">
+	<div className="row">
+		<div class="col-sm-6">
+        <h3><strong>Testimonial</strong></h3>
+        <div class="seprator"></div>
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-              
+              <!-- Wrapper for slides -->
               <div class="carousel-inner">
                 <div class="item active">
                   <div class="row" style="padding: 20px">
                     <button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
-                    <p class="testimonial_para">Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. 
-                    Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
-                    totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo en.</p><br>
+                    <p class="testimonial_para">Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo en.</p><br>
                     <div class="row">
                     <div class="col-sm-2">
-                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/jack.jpg" class="img-responsive" style="width: 80px"></img>
+                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/jack.jpg" class="img-responsive" style="width: 80px">
                         </div>
                         <div class="col-sm-10">
                         <h4><strong>Jack Andreson</strong></h4>
-                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span><br/>
+                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span><br>
                         <span>Officeal All Star Cafe</span>
                         </p>
                     </div>
@@ -36,26 +31,24 @@ import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
                   </div>
                 </div>
                <div class="item">
-                   <div class="row">
+                   <div class="row" style="padding: 20px">
                     <button style="border: none;"><i class="fa fa-quote-left testimonial_fa" aria-hidden="true"></i></button>
                     <p class="testimonial_para">Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo en.</p><br>
                     <div class="row">
                     <div class="col-sm-2">
-                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/kiara.jpg" class="img-responsive" style="width: 80px"></img>
+                        <img src="http://demos1.showcasedemos.in/jntuicem2017/html/v1/assets/images/kiara.jpg" class="img-responsive" style="width: 80px">
                         </div>
                         <div class="col-sm-10">
                         <h4><strong>Kiara Andreson</strong></h4>
-                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span>
-                        <br/>
+                        <p class="testimonial_subtitle"><span>Chlinical Chemistry Technologist</span><br>
                         <span>Officeal All Star Cafe</span>
                         </p>
-                    </div>
+                    </div><!--End of col-sm-2 -->
                     </div>
                   </div>
-                  </div>
-                </div>
+                </div><!--End of item -->
               </div>
-            </div>
+            </div><!--End of courosel -->
             <div class="controls testimonial_control pull-right">
                 <a class="left fa fa-chevron-left btn btn-default testimonial_btn" href="#carousel-example-generic"
                   data-slide="prev"></a>
@@ -64,15 +57,8 @@ import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons';
                   data-slide="next"></a>
               </div>
         </div>
-                        
-                      </div>
-
-                    </div>
-                    
-                </div>{/*End of container */}
-
-            </div>{/*End of row */}
-
+	</div>
+</div>
             
         </div>
     )
